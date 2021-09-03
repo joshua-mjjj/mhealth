b68eaaa7d4e70ee1fc473a149d76600ce11f2fc0
joshua-mjjj <joshuajosh423@gmail.com>
2021-09-03 12:23:50 +0300

    Making a few changes in selenuim.py

--- mHealth/selenium-whatsapp-script.py
+++ mHealth/selenium-whatsapp-script.py
@@ -60,21 +60,18 @@ if __name__ == '__main__': # protects from accidentally invoking the script
                 # path to browser cache in home
                 # /home/joshua/.config/google-chrome/Default
                 options = webdriver.ChromeOptions()
-                # options.add_argument(r'--user-data-dir=home/collins/.config/google-chrome/Default')
+                options.add_argument(r'--user-data-dir=home/joshua/.config/google-chrome/Default') # this is the path that allows what's up to sign in to your web and it has to been directed to you chrome default storage.
                 options.add_argument('--profile-directory=Default')
                 options.add_argument('--disable-popup-blocking')
 
                  # path where webdriver is stored
-                 #/home/collins/Downloads/Compressed
-                 # /home/joshua/Downloads/chromedriver_linux64/chromedriver
-                #chrome_browser = webdriver.Chrome(executable_path=r'/home/joshua/Downloads/chromedriver_linux64/chromedriver', options=options)
                 chrome_browser = webdriver.Chrome(executable_path=r'chromedriver_linux64/chromedriver', options=options)
                 print("done")
-                chrome_browser.execute_script("window.onbeforeunload = function() {};") 
+                chrome_browser.execute_script("window.onbeforeunload = function() {};")  
                 chrome_browser.get('https://web.whatsapp.com/')
-                time.sleep(5)
+                time.sleep(10)
                  
-                user_name_list = ['+256751964081', '+256787689679']
+                user_name_list = ['+256751964081']  # you can add names from your contact list or new numbers 
 
                 for user_name in user_name_list:
                     try:
@@ -84,12 +81,12 @@ if __name__ == '__main__': # protects from accidentally invoking the script
                     except NoSuchElementException as se:
                         # chat doesnot exist hence new chat
                         print("New number")
-                        chrome_browser.execute_script("window.onbeforeunload = function() {};") 
+                        chrome_browser.execute_script("window.onbeforeunload = function() {};")            
                         chrome_browser.get('https://web.whatsapp.com/send?phone={}&text&source&data&app_absent'.format(user_name))
                         # new_chat(user_name)
                         time.sleep(10)
 
-                    time.sleep(5)
+                    time.sleep(10)
                     message_box = chrome_browser.find_element_by_xpath('//div[@class="p3_M1"]')
                     print("find message bar..")
                     time.sleep(5)
@@ -103,10 +100,22 @@ if __name__ == '__main__': # protects from accidentally invoking the script
                     message_box.click()
                     # input_box.send_keys(Keys.ENTER)
                     print('message sent to: "{}"'.format(user_name))
-                    time.sleep(5) # time for last message to actually be sent....   ==================================================
+                    time.sleep(5) # time for last message to actually be sent....  
                     # chrome_browser.close()
                 #     # sys.exit()
                 chrome_browser.close()
                 sys.exit()
             today = date.today().strftime('%d/%m/%Y') # updating today
             time.sleep(10)  
+
+
+
+
+# <div class="_3HQNh _1Ae7k">
+
+# <button class="_4sWnG">
+# <span data-testid="send" data-icon="send" class=""><
+# svg viewBox="0 0 24 24" width="24" height="24" class="">
+# path fill="currentColor" d="M1.101 21.757L23.8 12.028 1.101 2.3l.
+# 011 7.912 13.623 1.816-13.623 1.817-.011 7.912z"></path></svg></
+# span></button></div>
